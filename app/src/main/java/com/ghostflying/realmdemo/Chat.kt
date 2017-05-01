91e05d7a15afa19cc35a8aa92b22ba8abdc1763e
GhostFlying <ghostflying@gmail.com>
2017-05-01 21:04:49 +0800

    add more

--- app/src/main/java/com/ghostflying/realmdemo/Chat.kt
+++ app/src/main/java/com/ghostflying/realmdemo/Chat.kt
@@ -6,6 +6,7 @@ import io.realm.annotations.PrimaryKey
 /**
  * Created by ghostflying on 2017/5/1.
  */
-open class Chat(@PrimaryKey open var chatId: Long = -1
+open class Chat(@PrimaryKey open var chatId: Long = -1,
+                open var chatTitle : String = ""
 ) : RealmObject() {
 }
